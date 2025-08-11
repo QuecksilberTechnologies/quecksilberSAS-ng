509634af9b0ba57295de9e2e382f69ed2be8d7c0
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-08-12 02:00:08 +0530

    Implement services section in 'Our Services' page with dynamic content and improved layout. Added service list with icons and descriptions, utilizing PrimeNG and Lucide components for enhanced UI.

--- src/app/pages/our-services/our-services.ts
+++ src/app/pages/our-services/our-services.ts
@@ -1,11 +1,64 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { PageHeader } from '@shared/components';
+import {
+  Bot,
+  Laptop,
+  Link,
+  Lock,
+  LucideAngularModule,
+  LucideIconData,
+  Phone,
+  Sparkle,
+} from 'lucide-angular';
+import { CardModule } from 'primeng/card';
+
+export interface OurService {
+  icon: LucideIconData;
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'qs-our-services',
-  imports: [PageHeader],
+  imports: [PageHeader, CardModule, LucideAngularModule],
   templateUrl: './our-services.html',
   styleUrl: './our-services.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class OurServices {}
+export default class OurServices {
+  serviceLists = signal<OurService[]>([
+    {
+      icon: Laptop,
+      title: 'Web Development',
+      description:
+        'Custom web applications and responsive websites built with modern technologies.',
+    },
+    {
+      icon: Phone, //'📱',
+      title: 'Mobile Apps',
+      description: 'Native and cross-platform mobile applications for iOS and Android.',
+    },
+    {
+      icon: Lock, //'🔒',
+      title: 'Cybersecurity',
+      description: 'Comprehensive security solutions to protect your digital assets.',
+    },
+    {
+      icon: Bot, //'🤖',
+      title: 'AI Agents',
+      description: 'Custom AI agents and chatbots to automate and enhance your business processes.',
+    },
+    {
+      icon: Sparkle, //'✨',
+      title: 'Vibe Coding',
+      description:
+        "Modern, aesthetic, and user-friendly interfaces that capture your brand's essence.",
+    },
+    {
+      icon: Link, // '🔗',
+      title: 'AI Integration',
+      description:
+        'Seamless integration of AI technologies into your existing systems and workflows.',
+    },
+  ]);
+}
