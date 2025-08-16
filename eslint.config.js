fd0dced08bd058ae8693a1d3a4badb1635cecfca
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-08-16 11:06:00 +0530

    Fixed - Lint error

--- eslint.config.js
+++ eslint.config.js
@@ -41,6 +41,7 @@ module.exports = tseslint.config(
       '@angular-eslint/prefer-signals': 'warn',
       '@angular-eslint/prefer-standalone': 'warn',
 
+
       // TypeScript best practices
       // '@typescript-eslint/array-type': ['warn'],
       // '@typescript-eslint/consistent-indexed-object-style': 'off',
@@ -53,19 +54,19 @@ module.exports = tseslint.config(
       //     accessibility: 'no-public',
       //   },
       // ],
-      // '@typescript-eslint/naming-convention': [
-      //   'warn',
-      //   {
-      //     selector: 'variable',
-      //     format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
-      //   },
-      // ],
-      // '@typescript-eslint/no-empty-function': 'warn',
-      // '@typescript-eslint/no-empty-interface': 'error',
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+        },
+      ],
+      '@typescript-eslint/no-empty-function': 'warn',
+      '@typescript-eslint/no-empty-interface': 'error',
       // '@typescript-eslint/no-explicit-any': 'warn',
       // '@typescript-eslint/no-inferrable-types': 'warn',
       // '@typescript-eslint/no-shadow': 'warn',
-      // '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
 
       // JavaScript best practices
       // eqeqeq: 'error',
@@ -85,7 +86,7 @@ module.exports = tseslint.config(
       // 'no-console': 'off',
       // 'no-new-wrappers': 'error',
       // 'no-useless-concat': 'error',
-      // 'no-var': 'error',
+      'no-var': 'error',
       // 'no-restricted-syntax': 'off',
       // 'no-shadow': 'error',
       // 'one-var': ['error', 'never'],
@@ -124,13 +125,15 @@ module.exports = tseslint.config(
           ],
         },
       ],
-      // '@angular-eslint/template/button-has-type': 'warn',
+      '@angular-eslint/template/button-has-type': 'warn',
       // '@angular-eslint/template/cyclomatic-complexity': ['warn', { maxComplexity: 10 }],
       // '@angular-eslint/template/eqeqeq': 'error',
       '@angular-eslint/template/prefer-control-flow': 'error',
-      // '@angular-eslint/template/prefer-ngsrc': 'warn',
-      // '@angular-eslint/template/prefer-self-closing-tags': 'warn',
-      // '@angular-eslint/template/use-track-by-function': 'warn',
+      '@angular-eslint/template/prefer-ngsrc': 'warn',
+      '@angular-eslint/template/prefer-self-closing-tags': 'warn',
+      '@angular-eslint/template/use-track-by-function': 'warn',
+      "@angular-eslint/template/interactive-supports-focus": "off",
+      "@angular-eslint/template/click-events-have-key-events": "off",
     }
   }
 );
