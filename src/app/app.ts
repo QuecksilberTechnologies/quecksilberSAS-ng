1f599427469be914d1961b1a2aae617ecc233746
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-08-16 17:01:05 +0530

    Added - Route progressbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,14 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '@shared/components';
 import { Footer } from '@shared/components/footer/footer';
+import { NgProgressbar } from 'ngx-progressbar';
+import { NgProgressRouter } from 'ngx-progressbar/router';
 import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, NgProgressbar, NgProgressRouter],
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
