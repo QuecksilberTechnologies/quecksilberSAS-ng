8a4f6a923e55d66241d682db70c913bb8cf1073c
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-08-16 16:49:38 +0530

    Added page title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 
+const titleSuffix = 'Quecksilber'; //environment.appName;
+
 export const routes: Routes = [
   {
     path: '',
@@ -9,29 +11,36 @@ export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./pages/home/home'),
+    title: `Home | ${titleSuffix}`,
   },
   {
     path: 'about',
     loadComponent: () => import('./pages/about/about'),
+    title: `About | ${titleSuffix}`,
   },
   {
     path: 'works',
     loadComponent: () => import('./pages/works/works'),
+    title: `Works | ${titleSuffix}`,
   },
   {
     path: 'services',
     loadComponent: () => import('./pages/our-services/our-services'),
+    title: `Services | ${titleSuffix}`,
   },
   {
     path: 'careers',
     loadComponent: () => import('./pages/careers/careers'),
+    title: `Careers | ${titleSuffix}`,
   },
   {
     path: 'blogs',
     loadComponent: () => import('./pages/blogs/blogs'),
+    title: `Blogs | ${titleSuffix}`,
   },
   {
     path: 'contact',
     loadComponent: () => import('./pages/contact/contact'),
+    title: `Contact | ${titleSuffix}`,
   },
 ];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'qs-home',
-  imports: [NgOptimizedImage, ButtonModule], 
+  imports: [NgOptimizedImage, ButtonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class Home {}
+export default class Home {
+  readonly #router = inject(Router);
+
+  handleNavClick(route: string) {
+    this.#router.navigate([route]);
+  }
+}
