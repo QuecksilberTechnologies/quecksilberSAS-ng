ee7b2344069ad111b71fe0c65f3a36f730c43266
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-09-05 00:42:23 +0530

    Updated careers page to include social media links for team members and improved UI layout. Added socialLinks property to Team interface for better data structure.

--- src/app/pages/careers/careers.ts
+++ src/app/pages/careers/careers.ts
@@ -2,7 +2,7 @@ import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { PageHeader } from '@shared/components';
 import { Team } from '@shared/interfaces';
-import { Linkedin, LucideAngularModule } from 'lucide-angular';
+import { Instagram, Linkedin, LucideAngularModule, Twitter } from 'lucide-angular';
 import { CardModule } from 'primeng/card';
 
 @Component({
@@ -18,39 +18,156 @@ export default class Careers {
       position: 'Chief Executive Officer (Founder)',
       image: 'img/team/avatar-person.svg',
       alt: 'Varun',
+      socialLinks: [
+        {
+          name: 'LinkedIn',
+          url: '',
+          icon: Linkedin,
+          ariaLabel: 'LinkedIn',
+        },
+        {
+          name: 'X',
+          url: '',
+          icon: Twitter,
+          ariaLabel: 'x',
+        },
+        {
+          name: 'Instagram',
+          url: '',
+          icon: Instagram,
+          ariaLabel: 'Instagram',
+        },
+      ],
     },
     {
       name: 'Poonam Gupta',
       position: 'Human Resources Manager',
       image: 'img/team/avatar-person.svg',
       alt: 'Poonam Gupta',
+      socialLinks: [
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/quecksilber-technologies-hr-06b095128',
+          icon: Linkedin,
+          ariaLabel: 'LinkedIn',
+        },
+        {
+          name: 'X',
+          url: '',
+          icon: Twitter,
+          ariaLabel: 'x',
+        },
+        {
+          name: 'Instagram',
+          url: '',
+          icon: Instagram,
+          ariaLabel: 'Instagram',
+        },
+      ],
     },
     {
       name: 'Deepak',
       position: 'Tech Team Lead',
       image: 'img/team/avatar-person.svg',
       alt: 'Deepak',
+      socialLinks: [
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/deepak-rajput-779b0654',
+          icon: Linkedin,
+          ariaLabel: 'LinkedIn',
+        },
+        {
+          name: 'X',
+          url: '',
+          icon: Twitter,
+          ariaLabel: 'x',
+        },
+        {
+          name: 'Instagram',
+          url: '',
+          icon: Instagram,
+          ariaLabel: 'Instagram',
+        },
+      ],
     },
     {
       name: 'Sujeet',
       position: 'Senior Frontend Developer',
       image: 'img/team/avatar-person.svg',
       alt: 'Sujeet',
+      socialLinks: [
+        {
+          name: 'LinkedIn',
+          url: '',
+          icon: Linkedin,
+          ariaLabel: 'LinkedIn',
+        },
+        {
+          name: 'X',
+          url: '',
+          icon: Twitter,
+          ariaLabel: 'x',
+        },
+        {
+          name: 'Instagram',
+          url: '',
+          icon: Instagram,
+          ariaLabel: 'Instagram',
+        },
+      ],
     },
     {
       name: 'Deepesh Gupta',
       position: 'Senior Backend Developer',
       image: 'img/team/avatar-person.svg',
       alt: 'Deepesh Gupta',
+      socialLinks: [
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/deepesh-gupta-06a436296',
+          icon: Linkedin,
+          ariaLabel: 'LinkedIn',
+        },
+        {
+          name: 'X',
+          url: '',
+          icon: Twitter,
+          ariaLabel: 'x',
+        },
+        {
+          name: 'Instagram',
+          url: '',
+          icon: Instagram,
+          ariaLabel: 'Instagram',
+        },
+      ],
     },
-
     {
       name: 'Abhishek',
       position: 'Senior DevOps Engineer',
       image: 'img/team/avatar-person.svg',
       alt: 'Abhishek',
+      socialLinks: [
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/abhishek-kanojia-58312716b',
+          icon: Linkedin,
+          ariaLabel: 'LinkedIn',
+        },
+        {
+          name: 'X',
+          url: '',
+          icon: Twitter,
+          ariaLabel: 'x',
+        },
+        {
+          name: 'Instagram',
+          url: '',
+          icon: Instagram,
+          ariaLabel: 'Instagram',
+        },
+      ],
     },
   ]);
-
-  readonly Linkedin = Linkedin;
 }
--- src/app/shared/interfaces/Team.ts
+++ src/app/shared/interfaces/Team.ts
@@ -1,6 +1,16 @@
+import { LucideIconData } from 'lucide-angular';
+
 export interface Team {
   name: string;
   position: string;
   image: string;
   alt: string;
+  socialLinks?: TeamSocialLink[];
+}
+
+export interface TeamSocialLink {
+  name: string;
+  url: string;
+  icon: LucideIconData;
+  ariaLabel: string;
 }
