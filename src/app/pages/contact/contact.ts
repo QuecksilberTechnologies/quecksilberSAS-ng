ddd8e8d6c7583b50f509ac0f61a5102273e626a9
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-09-06 22:43:52 +0530

    Enhanced contact page with a responsive form for user inquiries, including validation for required fields. Updated styles for improved layout and switched background color. Integrated necessary Angular modules for form handling and UI components.

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,10 +1,52 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PageHeader } from '@shared/components';
+import { AutoFocusModule } from 'primeng/autofocus';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'qs-contact',
-  imports: [PageHeader],
+  imports: [
+    PageHeader,
+    CardModule,
+    ReactiveFormsModule,
+    MessageModule,
+    AutoFocusModule,
+    InputTextModule,
+    TextareaModule,
+    ButtonModule,
+    // JsonPipe,
+  ],
   templateUrl: './contact.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class Contact {}
+export default class Contact implements OnInit {
+  readonly #fb = inject(FormBuilder);
+  readonly isSaving = signal<boolean>(false);
+  protected contactForm!: FormGroup;
+
+  async ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.contactForm = this.#fb.group({
+      userName: ['', Validators.required],
+      email: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  isInvalid(controlName: string) {
+    const control = this.contactForm.get(controlName);
+    return control?.invalid && control.touched;
+  }
+
+  onSend() {
+    console.log(this.contactForm.value);
+  }
+}
