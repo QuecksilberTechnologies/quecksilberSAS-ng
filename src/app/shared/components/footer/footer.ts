1e8e2e44757324975cdf460442c53af891963b29
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-09-07 01:06:51 +0530

    Implemented social media links in the footer component, enhancing user engagement. Updated footer layout for better spacing and added icons for LinkedIn, Twitter, and Instagram. Refactored footer TypeScript to include a signal for managing social link data.

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,10 +1,45 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Instagram, Linkedin, LucideAngularModule, LucideIconData, Twitter } from 'lucide-angular';
+
+export interface SocialLink {
+  name: string;
+  url: string;
+  icon: LucideIconData;
+  ariaLabel: string;
+}
 
 @Component({
   selector: 'qs-footer',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Footer {}
+export class Footer {
+  readonly socialLinks = signal<SocialLink[]>([
+    {
+      name: 'LinkedIn',
+      url: '',
+      icon: Linkedin,
+      ariaLabel: 'LinkedIn',
+    },
+    {
+      name: 'X',
+      url: '',
+      icon: Twitter,
+      ariaLabel: 'x',
+    },
+    {
+      name: 'Instagram',
+      url: '',
+      icon: Instagram,
+      ariaLabel: 'Instagram',
+    },
+    // {
+    //   name: 'Bsky',
+    //   url: 'https://bsky.app',
+    //   icon: Twitter,
+    //   ariaLabel: 'bsky',
+    // },
+  ]);
+}
