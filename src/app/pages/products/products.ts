3ad6ff54590590e7663daa36f831da283f91144c
Sujeet Jaiswara <sujeet@cleartrust.cc>
2025-09-26 13:47:05 +0530

    Added - Products page

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,12 +1,12 @@
-import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { PageHeader } from '@shared/components';
 import { BadgeModule } from 'primeng/badge';
 import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'qs-products',
-  imports: [PageHeader, CardModule, NgOptimizedImage, BadgeModule],
+  imports: [PageHeader, CardModule, BadgeModule, RouterModule],
   templateUrl: './products.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
